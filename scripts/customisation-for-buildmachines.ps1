75966830a0b7f75666c4d3b25da395331a3448c8
Florian Piesche <piesche@ccdc.cam.ac.uk>
2020-02-19 16:27:06 +0000

    Use ConvertTo-SecureString properly.

--- scripts/customisation-for-buildmachines.ps1
+++ scripts/customisation-for-buildmachines.ps1
@@ -1,10 +1,14 @@
 if ($Env:CUSTOMISE_FOR_BUILDMACHINE -eq "1") {
+    if (-Not (Test-Path Env:VAGRANT_USER_FINAL_PASSWORD)) {
+        Write-Output "VAGRANT_USER_FINAL_PASSWORD is not set!"
+        exit
+    }
     Write-Output "Setting final password for vagrant user..."
-    # Get the password passed in from the Packer provisioner and convert it to a SecureString
-    # This needs the -Force parameter to work with the -AsPlainText parameter because the password
-    # is not an encrypted string.
-    $SecurePassword = ($Env:VAGRANT_USER_FINAL_PASSWORD | ConvertTo-SecureString -AsPlainText -Force)
-    # Set the password for vagrant to the final password
+    # Get the password passed in from the Packer provisioner and convert it to a SecureString,
+    # then set it as the new password for the vagrant user.
+    # ConvertTo-SecureString needs the -Force parameter to work with the -AsPlainText
+    # parameter because the password is not an encrypted string.
+    $SecurePassword = ConvertTo-SecureString -AsPlainText -Force -String $Env:VAGRANT_USER_FINAL_PASSWORD
     Set-LocalUser -Name vagrant -Password $SecurePassword
 
     Write-Output "Setting autologon for vagrant user..."
