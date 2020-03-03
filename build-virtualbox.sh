11f7ff1d35e8d32e35d329c5a4796361a35ce924
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2020-03-03 14:21:44 +0000

    GPT disks, pvscsi on vmware, 80Gb main disks

--- build-virtualbox.sh
+++ build-virtualbox.sh
@@ -12,18 +12,23 @@ else
   VBOXMANAGE="vboxmanage"
 fi
 
+export AUTOUNATTEND_ISO=$PWD/output/autounattend.iso
+
 echo 'Cleaning existing output...'
 rm -rf ./output-virtualbox-iso
-rm -f ./output/autounattend.iso
+rm -f $AUTOUNATTEND_ISO
+rm -rf ./output/autounattend-iso-source
+
+echo 'Removing any left over disk images from previous builds...'
+rm -f ./builds.vmdk
+rm -f ./x_mirror.vmdk
+
 # rm -rf ./output/packer-windows-virtualbox
 EXISTINGVM="$( "$VBOXMANAGE" list vms | grep packer-virtualbox-iso | sed -e 's/\ .*//' | sed -e 's/"//g' )"
 if [[ $EXISTINGVM != "" ]]; then
   echo "Purging existing VM $EXISTINGVM"
   "$VBOXMANAGE" unregistervm $EXISTINGVM --delete
 fi
-echo 'Removing any left over disk images from previous builds...'
-rm -f ./builds.vmdk
-rm -f ./x_mirror.vmdk
 
 echo 'Creating output directory'
 mkdir -p output
@@ -31,6 +36,12 @@ mkdir -p output
 export VAGRANT_USER_FINAL_PASSWORD=vagrant
 sed -e "s#<Value>vagrant</Value>#<Value>$VAGRANT_USER_FINAL_PASSWORD</Value>#" unattend-floppy-scripts/unattend.xml.template > unattend-floppy-scripts/unattend.xml
 sed -e "s#<Value>vagrant</Value>#<Value>$VAGRANT_USER_FINAL_PASSWORD</Value>#" answer_files/server-2019/Autounattend.xml.template > answer_files/server-2019/Autounattend.xml
+
+echo 'copying scripts and autounattend files'
+mkdir -p ./output/autounattend-iso-source
+cp ./answer_files/server-2019/* ./output/autounattend-iso-source/
+cp ./unattend-floppy-scripts/* ./output/autounattend-iso-source/
+cp ./vmxnet3/* ./output/autounattend-iso-source/
 
 if [[ $(uname) == "Linux" ]]
 then
@@ -41,18 +52,18 @@ then
   fi
   echo "Creating iso image for autounattend.xml file"
   genisoimage -v -J -rational-rock -input-charset utf-8 -o - \
-    ./answer_files/server-2019 \
-    ./unattend-floppy-scripts \
-    ./vmxnet3 \
-    > ./output/autounattend.iso
+    output/autounattend-iso-source/ \
+    > $AUTOUNATTEND_ISO
 elif [[ $(uname) == "Darwin" ]]
 then
   echo "Creating iso image for autounattend.xml file"
-  hdiutil makehybrid -o ./output/autounattend.iso ./answer_files/server-2019 -iso -joliet
+  hdiutil makehybrid -o $AUTOUNATTEND_ISO output/autounattend-iso-source/ -iso -joliet
 else
   echo "Can't create iso image for autounattend.xml file"
   exit 1
 fi
+
+export PACKER_LOG=1
 
 echo 'Building base images'
 $PACKER build \
--- build-vmware.sh
+++ build-vmware.sh
@@ -10,15 +10,50 @@ else
   PACKER="packer"
 fi
 
+export AUTOUNATTEND_ISO=$PWD/output/autounattend.iso
+
+echo 'Cleaning existing output...'
+rm -rf ./output-vmware-iso
+rm -f $AUTOUNATTEND_ISO
+rm -rf ./output/autounattend-iso-source
+rm -rf ./output/packer-windows-2019-vmware
+
 echo 'creating output directory'
 mkdir -p output
-
-echo 'cleaning up intermediate output'
-rm -rf ./output//packer-centos-7.7-x86_64-vmware
 
 export VAGRANT_USER_FINAL_PASSWORD=vagrant
 sed -e "s#<Value>vagrant</Value>#<Value>$VAGRANT_USER_FINAL_PASSWORD</Value>#" ./unattend-floppy-scripts/unattend.xml.template > ./unattend-floppy-scripts/unattend.xml
 sed -e "s#<Value>vagrant</Value>#<Value>$VAGRANT_USER_FINAL_PASSWORD</Value>#" answer_files/server-2019/Autounattend.xml.template > answer_files/server-2019/Autounattend.xml
+
+echo 'copying scripts and autounattend files'
+mkdir -p ./output/autounattend-iso-source
+cp ./answer_files/server-2019/* ./output/autounattend-iso-source/
+cp ./unattend-floppy-scripts/* ./output/autounattend-iso-source/
+cp ./vmxnet3/* ./output/autounattend-iso-source/
+mkdir -p './output/autounattend-iso-source/$WinpeDriver$'
+cp -R ./pvscsi './output/autounattend-iso-source/$WinpeDriver$/'
+
+if [[ $(uname) == "Linux" ]]
+then
+  if [[ ! -f /usr/bin/genisoimage ]]
+  then
+    echo "Installing genisoimage, please enter your password"
+    sudo apt install genisoimage
+  fi
+  echo "Creating iso image for autounattend.xml file"
+  genisoimage -v -J -rational-rock -input-charset utf-8 -o - \
+    output/autounattend-iso-source/ \
+    > $AUTOUNATTEND_ISO
+elif [[ $(uname) == "Darwin" ]]
+then
+  echo "Creating iso image for autounattend.xml file"
+  hdiutil makehybrid -o $AUTOUNATTEND_ISO output/autounattend-iso-source/ -iso -joliet
+else
+  echo "Can't create iso image for autounattend.xml file"
+  exit 1
+fi
+
+export PACKER_LOG=1
 
 echo 'building base images'
 $PACKER build \
