5139ce1bd68dec408254770c7b71d0a186a771db
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2021-11-24 15:11:01 +0000

    Update initial vmware tools version NO_JIRA

--- scripts/vm-guest-tools.bat
+++ scripts/vm-guest-tools.bat
@@ -20,7 +20,7 @@ if exist "C:\Users\vagrant\windows.iso" (
 
 if not exist "C:\Windows\Temp\windows.iso" (
     echo "Downloading and extracting VMWare Tools..."
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/ws/16.1.0/17198959/windows/packages/tools-windows.tar', 'C:\Windows\Temp\vmware-tools.tar')" <NUL
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/ws/16.2.1/18811642/windows/packages/tools-windows.tar', 'C:\Windows\Temp\vmware-tools.tar')" <NUL
     cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\vmware-tools.tar -oC:\Windows\Temp"
     FOR /r "C:\Windows\Temp" %%a in (VMware-tools-windows-*.iso) DO REN "%%~a" "windows.iso"
     rd /S /Q "C:\Program Files (x86)\VMWare"
